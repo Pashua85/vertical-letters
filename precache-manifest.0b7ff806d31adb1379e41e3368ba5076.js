41b4ce7299f4554f1f2e7b0d66e15b20654dff35
Pashua85 <pavellashuk85@gmail.com>
2020-10-22 12:17:50 +0500

    Updates

---
+++ precache-manifest.0b7ff806d31adb1379e41e3368ba5076.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25f5a8f926e7cdd4ff02bc640cee905",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2b63ecda0224e6de900",
+    "url": "/static/css/main.9864156b.chunk.css"
+  },
+  {
+    "revision": "a5256c0ae7fe3c1c3425",
+    "url": "/static/js/2.be6df2eb.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.be6df2eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b63ecda0224e6de900",
+    "url": "/static/js/main.6bed7ed4.chunk.js"
+  },
+  {
+    "revision": "73f9fb4287d3af5ef61e",
+    "url": "/static/js/runtime-main.ea659a83.js"
+  }
+]);
